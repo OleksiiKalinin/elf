86b21080271704b155386327997cbdde0339158a
OleksiiKalinin <73401289+OleksiiKalinin@users.noreply.github.com>
2023-10-11 14:48:11 +0200

    keystore

--- src/components/organismes/GoogleMap.tsx
+++ src/components/organismes/GoogleMap.tsx
@@ -1,35 +1,48 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { FC, useEffect, useRef, useState } from 'react';
-import { Dimensions, StyleSheet, TouchableOpacity, View } from 'react-native';
+import React, { FC, HTMLFactory, useEffect, useRef, useState } from 'react';
+import { Dimensions, Platform, StyleSheet, TouchableOpacity, View } from 'react-native';
 import geocoder from 'react-native-geocoder-reborn';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
-import markerIcon from '../../assets/images/marker.png';
+import MapView, { MapMarker, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { AddressType } from '../../store/reducers/types';
 import SvgIcon from '../atoms/SvgIcon';
 import Colors from '../../colors/Colors';
 import Button from '../molecules/Button';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
+import Autocomplete from "react-google-autocomplete";
+
+const Map = MapView as (typeof MapView & {Marker: typeof MapMarker})
+if (Platform.OS === 'android' || Platform.OS === 'ios') {
+    Map.Marker = Marker;
+}
+
+const markerIcon: any = 'https://drive.google.com/uc?id=18XoaiN-bJE9zslCbZNk6xpRCVLdikwGr&export=download';
 
 const GoogleMap: FC<{ callback: (address: AddressType) => void, initialAddress: AddressType | null, hideControls?: boolean }> = ({ callback, initialAddress, hideControls = false }) => {
     const [location, setLocation] = useState(initialAddress);
-    const GoogleInputRef = useRef<any>(null);
+    const [webInputValue, setWebInputValue] = useState<string>(location?.formattedAddress || '');
+    const NativeInputRef = useRef<any>(null);
+    const WebInputRef = useRef<HTMLInputElement | null>(null);
     const { windowSizes } = useTypedSelector(s => s.general);
     // const navigation = useNavigation();
 
     useEffect(() => {
-        if (location) GoogleInputRef.current?.setAddressText(location.formattedAddress);
+        if (location) NativeInputRef.current?.setAddressText(location.formattedAddress);
     }, []);
 
     useEffect(() => {
-        console.log(windowSizes);
-    }, [windowSizes]);
+        console.log(location);
+    }, [location]);
+
+    // useEffect(() => {
+    //     console.log(windowSizes);
+    // }, [windowSizes]);
 
     return (
         <View style={{ backgroundColor: Colors.Basic100, flex: 1, height: 700 }}>
             <View style={styles.Wrapper}>
-                <GooglePlacesAutocomplete
-                    ref={GoogleInputRef}
+                {(Platform.OS === 'ios' || Platform.OS === 'android') && <GooglePlacesAutocomplete
+                    ref={NativeInputRef}
                     placeholder="Wpisz lokalizację"
                     onPress={(data) => {
                         console.log(data);
@@ -63,26 +76,19 @@ const GoogleMap: FC<{ callback: (address: AddressType) => void, initialAddress: 
                                 alignSelf: 'flex-start',
                                 paddingRight: 10
                             }}
-                            onPress={!!GoogleInputRef.current?.getAddressText() ? () => {
-                                GoogleInputRef.current?.setAddressText('');
+                            onPress={!!NativeInputRef.current?.getAddressText() ? () => {
+                                NativeInputRef.current?.setAddressText('');
                                 setLocation(null);
                             } : undefined}
                         >
-                            <SvgIcon icon={!!GoogleInputRef.current?.getAddressText() ? "crossBig" : 'search'} />
+                            <SvgIcon icon={!!NativeInputRef.current?.getAddressText() ? "crossBig" : 'search'} />
                         </TouchableOpacity>
                     )}
-                    fetchDetails
+                    // fetchDetails
                     query={{
                         key: 'AIzaSyBLA1spwwoOjY2rOvMliOBc2C87k6ZOJ_s',
                         language: 'pl',
                         components: 'country:pl',
-                    }}
-                    requestUrl={{
-                        useOnPlatform: 'web',
-                        url: 'https://maps.googleapis.com/maps/api',
-                        //   headers: {
-                        //     Authorization: `an auth token`, // if required for your proxy
-                        //   },
                     }}
                     enablePoweredByContainer={false}
                     // currentLocation={true}
@@ -107,9 +113,57 @@ const GoogleMap: FC<{ callback: (address: AddressType) => void, initialAddress: 
                             textAlignVertical: 'center',
                         },
                     }}
-                />
+                />}
+                {Platform.OS === 'web' && <View style={{ flexDirection: 'row', width: '100%', height: 42 }}>
+                    <TouchableOpacity
+                        style={{
+                            height: 42,
+                            width: 35,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            alignSelf: 'flex-start',
+                        }}
+                    // onPress={() => navigation.goBack()}
+                    >
+                        <SvgIcon icon="arrowLeft" />
+                    </TouchableOpacity>
+                    <Autocomplete
+                        ref={WebInputRef}
+                        style={{ flex: 1, height: '100%', border: 0, fontSize: 16, fontWeight: '600', paddingLeft: 15 }}
+                        onPlaceSelected={(place) => {
+                            geocoder.geocodeAddress(place.formatted_address)
+                                .then(res => setLocation(res[0] as unknown as AddressType))
+                                .catch(err => console.log(err));
+                        }}
+                        defaultValue={webInputValue}
+                        options={{
+                            types: ['address'],
+                            componentRestrictions: { country: 'pl' }
+                        }}
+                        //@ts-ignore
+                        onChange={(e) => setWebInputValue(e.target.value || '')}
+                    />
+                    <TouchableOpacity
+                        style={{
+                            height: 42,
+                            width: 35,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            alignSelf: 'flex-start',
+                            paddingRight: 10
+                        }}
+                        onPress={!!webInputValue ? () => {
+                            //@ts-ignore
+                            if(WebInputRef.current) WebInputRef.current.value = '';
+                            setWebInputValue('');
+                            setLocation(null);
+                        } : undefined}
+                    >
+                        <SvgIcon icon={!!webInputValue ? "crossBig" : 'search'} />
+                    </TouchableOpacity>
+                </View>}
             </View>
-            <MapView
+            <Map
                 customMapStyle={[
                     {
                         featureType: 'poi',
@@ -124,44 +178,44 @@ const GoogleMap: FC<{ callback: (address: AddressType) => void, initialAddress: 
                 // style={[styles.Map, { height: windowSizes.height }]}
                 provider={PROVIDER_GOOGLE} // remove if not using Google Maps
                 region={{
-                    latitude: location?.position?.lat || 52.229838,
-                    longitude: location?.position?.lng || 21.011713,
-                    latitudeDelta: 0.015,
-                    longitudeDelta: 0.0121,
+                    latitude: location?.position?.lat || 52.087696,
+                    longitude: location?.position?.lng || 19.708369,
+                    latitudeDelta: location?.position ? 0.02 : 2.2,
+                    longitudeDelta: location?.position ? 0.02 : 2.2,
                 }}>
-                {/* <Marker
-                    // image={markerIcon}
-                    // draggable={!hideControls}
-                    coordinate={{
-                        latitude: location?.position?.lat || 52.229838,
-                        longitude: location?.position?.lng || 21.011713
-                    }}
-                    // onDragEnd={({ nativeEvent: { coordinate: { latitude, longitude } } }) => {
-                    //     if (location) {
-                    //         setLocation({
-                    //             ...location,
-                    //             position: { lat: latitude, lng: longitude }
-                    //         });
-                    //     }
-                    // }}
-                /> */}
-                {/* {!!location?.position?.lat && !!location?.position?.lng && <Marker
+                {!!location?.position?.lat && !!location?.position?.lng && <Map.Marker
                     image={markerIcon}
+                    icon={markerIcon}
                     draggable={!hideControls}
                     coordinate={{
                         latitude: location?.position.lat,
                         longitude: location?.position.lng
                     }}
-                    onDragEnd={({ nativeEvent: { coordinate: { latitude, longitude } } }) => {
-                        if (location) {
-                            setLocation({
-                                ...location,
-                                position: { lat: latitude, lng: longitude }
-                            });
+                    onDragEnd={(e: any) => {
+                        let [lat, lng]: (number | null)[] = [null, null];
+                        if (Platform.OS === 'android' || Platform.OS === 'ios') {
+                            const { nativeEvent: { coordinate: { latitude, longitude } } } = e;
+                            [lat, lng] = [latitude, longitude];
+                        } else if (Platform.OS === 'web' && e.latLng) {
+                            [lat, lng] = [e.latLng?.lat() || null, e.latLng?.lng() || null];
                         }
+                        console.log([lat, lng]);
+                        if (lat && lng) {
+                            geocoder.geocodePosition({lat, lng})
+                            .then(res => console.log(res[0]))
+                            // .then(res => setLocation(res[0] as unknown as AddressType))
+                            // .catch(err => console.log(err));
+                        }
+                        // if (location) {
+                        //     setLocation({
+                        //         ...location,
+                        //         position: { lat: latitude, lng: longitude }
+                        //     });
+                        // }
                     }}
-                />} */}
-            </MapView>
+                    // onDragEnd={(e) => console.log(e.latLng?.lng())}
+                />}
+            </Map>
             {!hideControls && <View style={{ position: 'absolute', bottom: 0, left: 0, zIndex: 100, width: '100%' }}>
                 <Button
                     variant="primary"
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -80,7 +80,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           __html: "document.documentElement.classList.add('t_unmounted')",
         }}
       />
-      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBLA1spwwoOjY2rOvMliOBc2C87k6ZOJ_s"></script>
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBLA1spwwoOjY2rOvMliOBc2C87k6ZOJ_s&libraries=places&language=pl&region=pl"></script>
       <Providers>{contents}</Providers>
     </>
   );
