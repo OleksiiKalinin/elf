b6dadb0541ba38966e0f60b18616366b725d0096
Rrqa Tyrjij <lukasz.wieckowski.inf@gmail.com>
2023-11-10 17:27:56 +0100

    Update Questions.

--- src/screens/MenuScreens/QuestionsScreen.tsx
+++ src/screens/MenuScreens/QuestionsScreen.tsx
@@ -1,20 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, View} from 'react-native';
 import Colors from '../../colors/Colors';
 import ScreenHeaderProvider from '../../components/organismes/ScreenHeaderProvider';
 import Typography from '../../components/atoms/Typography';
 import Button from '../../components/molecules/Button';
-import { ScrollView } from '../../components/molecules/ScrollView';
+import {ScrollView} from '../../components/molecules/ScrollView';
 import useRouter from '../../hooks/useRouter';
 import CornerCircleButton from '../../components/molecules/CornerCircleButton';
-import { QuestionsCategoryType, UserQuestionsType } from '../../store/reducers/types';
-import { Separator } from 'tamagui';
-import { MenuStackParamList } from '../../navigators/MenuNavigator';
-import { createParam } from 'solito';
-import { useTypedSelector } from '../../hooks/useTypedSelector';
+import {
+  QuestionsCategoryType,
+  UserQuestionsType,
+} from '../../store/reducers/types';
+import {Separator} from 'tamagui';
+import {MenuStackParamList} from '../../navigators/MenuNavigator';
+import {createParam} from 'solito';
+import {useTypedSelector} from '../../hooks/useTypedSelector';
 import SvgIcon from '../../components/atoms/SvgIcon';
-import { useActions } from '../../hooks/useActions';
-import { cloneDeep } from 'lodash';
+import {useActions} from '../../hooks/useActions';
+import {cloneDeep} from 'lodash';
+import {el} from 'react-native-paper-dates';
 
 const UserList: UserQuestionsType = {
   id: '1',
@@ -26,15 +30,18 @@ const UserList: UserQuestionsType = {
       questions: [
         {
           id: '11',
-          question: 'Jakie były Pana/Pani doświadczenia na podobnym stanowisku pracy?',
+          question:
+            'Jakie były Pana/Pani doświadczenia na podobnym stanowisku pracy?',
         },
         {
           id: '22',
-          question: 'Jaki był Pana/Pani największy sukces na poprzednim stanowisku pracy?',
+          question:
+            'Jaki był Pana/Pani największy sukces na poprzednim stanowisku pracy?',
         },
         {
           id: '33',
-          question: 'Jaki projekt zawodowy szczególnie Pana/Panią zainspirował? Z czego to wynikało?',
+          question:
+            'Jaki projekt zawodowy szczególnie Pana/Panią zainspirował? Z czego to wynikało?',
         },
       ],
     },
@@ -52,7 +59,8 @@ const UserList: UserQuestionsType = {
         },
         {
           id: '66',
-          question: 'Czy posiada Pan/Pani jakieś certyfikaty związane ze swoim stanowiskiem pracy?',
+          question:
+            'Czy posiada Pan/Pani jakieś certyfikaty związane ze swoim stanowiskiem pracy?',
         },
       ],
     },
@@ -62,7 +70,8 @@ const UserList: UserQuestionsType = {
       questions: [
         {
           id: '77',
-          question: 'Jakie umiejętności uważa Pan/Pani za najważniejsze na swoim stanowisku pracy?',
+          question:
+            'Jakie umiejętności uważa Pan/Pani za najważniejsze na swoim stanowisku pracy?',
         },
         {
           id: '88',
@@ -94,7 +103,8 @@ const UserList: UserQuestionsType = {
     },
   ],
 };
-const { useParam } = createParam<NonNullable<MenuStackParamList['default']['QuestionsScreen']>>();
+const {useParam} =
+  createParam<NonNullable<MenuStackParamList['default']['QuestionsScreen']>>();
 
 const QuestionsScreen: React.FC = () => {
   const {replace} = useRouter();
@@ -103,96 +113,126 @@ const QuestionsScreen: React.FC = () => {
   const [subView] = useParam('subView');
   const [index, setIndex] = useState<number | null>(null);
   const [selectedList, setSelectedList] = useState<UserQuestionsType>();
-  const { userQuestions } = useTypedSelector(state => state.general);
-  const { setSwipeablePanelProps, setUserQuestions } = useActions();
-
-  useEffect(()=> {
-    if(id){
-      const index = userQuestions.findIndex(item => item.id === id);
-      if(index >= 0){
-        setIndex(index);
-      } else{
-        router.push({ stack: 'MenuStack', screen: 'QuestionsListScreen', params: undefined});
-      };
-    };
-  },[id]);
+  const {userQuestions} = useTypedSelector(state => state.general);
+  const {setSwipeablePanelProps, setUserQuestions} = useActions();
 
   useEffect(() => {
-    if(id){
-      setSwipeablePanelProps((() => {
-        if (subView === 'options') return {
-          title: 'Co robimy tym razem?',
-          closeButton: true,
-          buttons: [
-            {
-              children: 'Edytuj listę',
-              icon: <SvgIcon icon='pencil'/>,
-              closeAction: 'props-null',
-              onPress: () => replace({ stack: 'MenuStack', screen: 'QuestionEditorScreen', params: {id: id} })
-            },
-            {
-              children: 'Usuń listę',
-              icon: <SvgIcon icon='crossBig' />,
-              closeAction: 'props-null',
-              onPress: () => {deleteList(), replace({ stack: 'MenuStack', screen: 'QuestionsListScreen', params: undefined })}
-            },
-          ],
-        }
-        return null;
-      })());
+    if (id) {
+      const index = userQuestions.findIndex(item => item.id === id);
+      if (index >= 0) {
+        setIndex(index);
+      } else {
+        router.push({
+          stack: 'MenuStack',
+          screen: 'QuestionsListScreen',
+          params: undefined,
+        });
+      }
+    }
+  }, [id]);
+
+  useEffect(() => {
+    if (id) {
+      setSwipeablePanelProps(
+        (() => {
+          if (subView === 'options')
+            return {
+              title: 'Co robimy tym razem?',
+              closeButton: true,
+              buttons: [
+                {
+                  children: 'Edytuj listę',
+                  icon: <SvgIcon icon="pencil" />,
+                  closeAction: 'props-null',
+                  onPress: () =>
+                    replace({
+                      stack: 'MenuStack',
+                      screen: 'QuestionEditorScreen',
+                      params: {id: id},
+                    }),
+                },
+                {
+                  children: 'Usuń listę',
+                  icon: <SvgIcon icon="crossBig" />,
+                  closeAction: 'props-null',
+                  onPress: () => {
+                    deleteList(),
+                      replace({
+                        stack: 'MenuStack',
+                        screen: 'QuestionsListScreen',
+                        params: undefined,
+                      });
+                  },
+                },
+              ],
+            };
+          return null;
+        })(),
+      );
     }
   }, [subView]);
 
-  useEffect(()=> {
+  useEffect(() => {
     const index = userQuestions.findIndex(item => item.id === id);
     setSelectedList(userQuestions[index]);
-  },[])
+  }, []);
 
-  const deleteList = () =>{
-    if(index !== null){
-      const newList = cloneDeep(userQuestions);
-      setUserQuestions(newList.splice(index, 1));
-    };
+  const deleteList = () => {
+    if (index !== null) {
+      setUserQuestions([
+        ...userQuestions.slice(0, index),
+        ...userQuestions.slice(index + 1),
+      ]);
+    }
   };
 
   const handleSubView = () => {
-    if(id){
-      router.push({ stack: 'MenuStack', screen: 'QuestionsScreen', params: {id: id, subView: 'options'}});
-    };
+    if (id) {
+      router.push({
+        stack: 'MenuStack',
+        screen: 'QuestionsScreen',
+        params: {id: id, subView: 'options'},
+      });
+    }
   };
 
   return (
     <>
-      {(id && index !== null) &&
-        <ScreenHeaderProvider 
-          title={selectedList ? selectedList.name : ''} 
+      {id && index !== null && (
+        <ScreenHeaderProvider
+          title={selectedList ? selectedList.name : ''}
           mainTitlePosition="flex-start"
-          actions={[{icon: 'threeDots', onPress: handleSubView}]}
-        >
-          <ScrollView style={{ backgroundColor: Colors.Basic100 }}>
-          <View style={styles.ListContainer}>
-            {selectedList?.list.map(({ id, category, questions }) =>
+          actions={[{icon: 'threeDots', onPress: handleSubView}]}>
+          <ScrollView style={{backgroundColor: Colors.Basic100}}>
+            <View style={styles.ListContainer}>
+              {selectedList?.list.map(({id, category, questions}) => (
                 <View key={id} style={styles.CategoryContainer}>
-                  <Typography size={20} weight='Bold'>
+                  <Typography size={20} weight="Bold">
                     {category}
                   </Typography>
                   <View style={styles.Questions}>
-                    {questions.map(({ id, question }, i) => 
+                    {questions.map(({id, question}, i) => (
                       <>
                         {i === 0 && <Separator />}
                         <View key={id} style={styles.Question}>
-                          <Typography weight='Bold' variant='small' style={styles.QuestionNumber}>{i+1}</Typography>
-                          <Typography variant='small'>{question}</Typography>
+                          <Typography
+                            weight="Bold"
+                            variant="small"
+                            style={styles.QuestionNumber}>
+                            {i + 1}
+                          </Typography>
+                          <Typography variant="small">{question}</Typography>
                         </View>
-                      <Separator />
-                    </>)}
+                        <Separator />
+                      </>
+                    ))}
                   </View>
                 </View>
-              )}
+              ))}
             </View>
           </ScrollView>
         </ScreenHeaderProvider>
-      }
+      )}
     </>
   );
 };
@@ -203,21 +243,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
   },
   CategoryContainer: {
-    marginBottom: 24
+    marginBottom: 24,
   },
   Questions: {
-    marginTop: 10
+    marginTop: 10,
   },
   Question: {
-    flexDirection: 'row', 
-    paddingVertical: 17
+    flexDirection: 'row',
+    paddingVertical: 17,
   },
   QuestionNumber: {
-    width: 12
+    width: 12,
   },
-  QuestionContent: {
-
-  },
+  QuestionContent: {},
 });
 
 export default QuestionsScreen;
